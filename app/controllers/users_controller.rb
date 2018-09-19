37e37f371fb5d46c6e03650bf1965a2c3dccb4aa
Kazuki Kobayashi <kaki.daylily@gmail.com>
2018-09-19 14:29:29 +0900

    リスト7.2.3 直前まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  
 
   def create
-    @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
       # 保存の成功をここで扱う。
     else
       render 'new'
     end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,:password_confirmation)
+    end
 end
