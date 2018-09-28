aafd08fd9f09fdee065f68ca9a4324028263a7ef
Kazuki Kobayashi <kaki.daylily@gmail.com>
2018-09-28 17:38:47 +0900

    12.1.2　終了まで

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ get '/login', to: 'sessions#new'
 post '/login', to: 'sessions#create'
 delete '/logout', to: 'sessions#destroy'
 resources :users
+resources :account_activations, only: [:edit]
+resources :password_resets,     only: [:new, :create, :edit, :update]
+
+
 end
